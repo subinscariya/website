33ff4bab7abe807d39a6e695e8760e510dc87e7b
subinscariya <65583712+subinscariya@users.noreply.github.com>
2020-09-22 13:21:26 +0530

    Add files via upload

---
+++ b.js
@@ -0,0 +1,8 @@
+function first(){
+
+    if(document.getElementById("username").value=="subin" && document.getElementById("password").value==1234)
+    {
+        alert("sucess");
+        
+    }
+}
